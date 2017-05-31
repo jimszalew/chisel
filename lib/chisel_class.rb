0a73b924f9a26fc737532569a3a4849f39260d61
Jim Szalewski <jim.szalewski@gmail.com>
2017-05-31 14:32:05 -0600

    code not functional, attempt at converting and writing to a new file

--- lib/chisel_class.rb
+++ lib/chisel_class.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Chisel
 
   attr_reader :input_file,
@@ -8,6 +10,15 @@ class Chisel
     @markdown   = File.read(input_file).chomp
     @input_file = input_file
     @output     = output
-    write_to_file(convert)
+  end
+
+  def write_to_file(output)
+    renderer  = Redcarpet::Render::HTML.new
+    engine    = Redcarpet::Markdown.new(renderer)
+    md_source = input_file
+    new_file = File.new(output, "w+")
+    new_file.write(engine.render(md_source))
   end
 end
+binding.pry
+""
